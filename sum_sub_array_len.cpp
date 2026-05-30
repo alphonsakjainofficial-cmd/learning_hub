02a231170508eadc1cbf6f038ee1302edd79e849
alphonsakjainofficial-cmd <alphonsakjainofficial@gmail.com>
2026-05-30 18:36:35 +0530

    sliding window solution for subarray length
    Implement sliding window approach to find the length of the largest subarray with a given sum.

---
+++ sum_sub_array_len.cpp
@@ -0,0 +1,24 @@
+// Sliding window approach
+#include <iostream>
+
+int main() {
+    int SIZE = 8; 
+    int arr[SIZE] = {1, 2, 3, 2, 1, 2, 1, 2};
+    int K = 5;
+    int maxlen = 0, sum = 0, right = 0, left = 0;
+    for (; right < SIZE; right++)
+    {
+        sum += arr[right];
+        while (sum > K && left <= right)
+        {
+            sum -= arr[left];
+            left++;
+        }
+        if (sum == K)
+        {
+            maxlen = std::max(maxlen, right - left + 1);
+        }
+    }
+    std::cout << "Length of the largest subarray with sum"<<K<<" is "<<maxlen;
+    return 0;
+}
